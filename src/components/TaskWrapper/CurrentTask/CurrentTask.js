93db7922c02d6beaa7bb20fef5e5e1f75ebabe2c
shanerwu <yunwu0128@gmail.com>
2019-11-29 14:58:36 +0800

    Rename project

--- src/components/TaskWrapper/CurrentTask/CurrentTask.js
+++ src/components/TaskWrapper/CurrentTask/CurrentTask.js
@@ -2,15 +2,17 @@ import React from 'react';
 import styles from './CurrentTask.module.css';
 
 const currentTask = (props) => (
-    <div className={styles.currentTask}>
-        <div className={styles.checkBox} />
-        <div className={styles.todo}>
-            <p>THE FIRST THING TO DO TODAY</p>
+    <>
+        <div className={styles.currentTask}>
+            <div className={styles.checkBox} />
+            <div className={styles.task}>
+                <p>THE FIRST THING TO DO TODAY</p>
+            </div>
         </div>
         <div className={styles.time}>
             25:00
         </div>
-    </div>
+    </>
 );
 
 export default currentTask;
